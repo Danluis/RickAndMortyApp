9eb24542d69af77503d8395f0a3e4982bec2ea4a
Danluis <danluis2a25@gmail.com>
2024-03-13 16:56:30 -0400

    RickAndMortyApp

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CharacterList from "./components/CharacterList";
+
+export default function App(){
+
+  return(
+    <div>
+      <h1 className="text-3xl font-bold underline">Rick and Morty</h1>
+      <CharacterList/>
+    </div>
+  )
+}
